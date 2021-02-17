831e76dc0bb257759e601300d1d4f11287fd466c
Michael Sheinman Orenstrakh <michael092001@gmail.com>
2021-02-17 16:48:24 -0500

    experimenting

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
+
   return <Component {...pageProps} />;
+
+  
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -4,8 +4,32 @@ const app = express();
 
 const port = 3030;
 
+const data = [
+  {
+    name: "Monday",
+    cases: 90,
+  },
+  {
+    name: "Tuesday",
+    cases: 80,
+  },
+  {
+    name: "Wednesday",
+    cases: 10,
+  },
+  {
+    name: "Thursday",
+    cases: 50,
+  },
+  {
+    name: "Friday",
+    cases: 70,
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("Foo Bar");
+  res.send(data);
 });
+
 
 app.listen(port, () => console.log(`server listening on port ${port}`));
