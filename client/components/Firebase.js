b3ed3074b6aaf7fc95a5873c8cbb599c56f1e0d5
Aaron Goidel <acgoidel@gmail.com>
2021-02-18 12:14:00 -0500

    feat(auth): add auth provider

---
+++ client/components/Firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDEXsuNMKL6retgLOHO_K_E8EjUawDYoaY",
+  authDomain: "track-19-ae4d5.firebaseapp.com",
+  projectId: "track-19-ae4d5",
+  storageBucket: "track-19-ae4d5.appspot.com",
+  messagingSenderId: "703806945805",
+  appId: "1:703806945805:web:dacbeec4c7bb5166147344",
+  measurementId: "G-3RX3NJN5LF",
+};
+
+// Initialize Firebase
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (err) {
+  if (!/already exists/.test(err.message)) {
+    console.error("Firebase initialization error", err.stack);
+  }
+}
+
+// firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+export const auth = firebase.auth();
+export default firebase;
